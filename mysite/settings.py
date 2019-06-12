5d493bdb652fff765e8122ef11f816bb26a8d25d
王四爷 <a1553342201@qq.com>
2019-06-12 16:25:15 +0800

    加入xadmin组件

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'xadmin',
+    'crispy_forms',
     'api.apps.ApiConfig',
     'rest_framework',
 ]
@@ -107,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path,include,re_path
 from api import urls
+from django.conf.urls import url,include
+
+# from django.contrib import admin 注释admin
+import xadmin
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('xadmin/', xadmin.site.urls),
     re_path('api/(?P<version>[v1|v2]+)/', include('api.urls')),
 ]
